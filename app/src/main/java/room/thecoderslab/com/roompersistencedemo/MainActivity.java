abf1f822c12dd5429a9e300d754e280331c9ea01
Ashish Tiwari <thecoderslab@users.noreply.github.com>
2018-05-05 02:23:51 +1000

    normal fixes

--- app/src/main/java/room/thecoderslab/com/roompersistencedemo/MainActivity.java
+++ app/src/main/java/room/thecoderslab/com/roompersistencedemo/MainActivity.java
@@ -1,18 +1,14 @@
 package room.thecoderslab.com.roompersistencedemo;
 
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String DATABASE_NAME="movie_db";
+
     private MovieDatabase movieDatabase;
     int count;
 
@@ -21,21 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //creating instance of movieDatabase
-        movieDatabase = Room.databaseBuilder(getApplicationContext(),MovieDatabase.class,DATABASE_NAME)
-                        .fallbackToDestructiveMigration()
-                        // .allowMainThreadQueries() //use this to allow query in main thread(not recommended may cause ANR)
-                        .build();
 
-        //adding button dynamically in layout
-        Button button = new Button(this);
-        button.setText(R.string.button_label_text);
+        FloatingActionButton fab = findViewById(R.id.fab);
 
-        ConstraintLayout layout = findViewById(R.id.constraintLayout);
-        layout.addView(button);
+        movieDatabase = MovieDatabase.getDatabase(this);
 
-       //setting up onclick listener for button click
-        button.setOnClickListener(new View.OnClickListener() {
+        //insert into database when floating button in pressed
+        fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -44,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Movies movie = new Movies();
-                        movie.setMovieID(21);
+                        movie.setMovieID(21 + 1);
                         movie.setMovieTitle("Avengers");
                         movie.setMovieOverview("Half of the world is wiped out");
 
@@ -53,19 +41,11 @@ public class MainActivity extends AppCompatActivity {
                         count = movieDatabase.movieDaoAccess().getCount();
 
                         Log.d("Count","rows = "+count);
-
                     }
                 }).start();
-
             }
         });
-
-
-
-
-
-
-
-
     }
+
 }
+
--- app/src/main/java/room/thecoderslab/com/roompersistencedemo/MovieDatabase.java
+++ app/src/main/java/room/thecoderslab/com/roompersistencedemo/MovieDatabase.java
@@ -1,10 +1,33 @@
 package room.thecoderslab.com.roompersistencedemo;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {Movies.class} , version = 3 , exportSchema = false)
 public abstract class MovieDatabase extends RoomDatabase {
+
+    private static MovieDatabase instance;
+    private static final String DATABASE_NAME = "movie_db";
+
+    //creating singleton
+    public static MovieDatabase getDatabase(final Context context) {
+        if (instance == null) {
+            synchronized (MovieDatabase.class) {
+                if (instance == null) {
+
+                    instance = Room.databaseBuilder(context.getApplicationContext(), MovieDatabase.class, DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+                            // .allowMainThreadQueries() //use this to allow query in main thread(not recommended may cause ANR)
+                            .build();
+                }
+            }
+
+        }
+        return instance;
+
+    }
 
     public abstract MovieDaoAccess movieDaoAccess();
 
